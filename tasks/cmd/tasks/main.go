fa7ecdbad5a906b20d3eda1926bf4e04cc00e9cd
freitzzz <1160907@isep.ipp.pt>
2021-07-27 12:34:34 +0100

    Runnable service on main

--- tasks/cmd/tasks/main.go
+++ tasks/cmd/tasks/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	http "github.com/freitzzz/sword-health-technical-challenge/tasks/internal/http"
+	"github.com/labstack/echo/v4"
+)
+
 func main() {
-	println("Hello world")
+
+	// Echo instance
+	e := echo.New()
+
+	http.RegisterHandlers(e)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":80"))
 }
