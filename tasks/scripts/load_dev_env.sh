805f2881c49c08dd9a5bb00413a5df2a5eee0b36
freitzzz <1160907@isep.ipp.pt>
2021-07-27 17:50:36 +0100

    Added script to load dev environment variables

---
+++ tasks/scripts/load_dev_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# General
+
+export environment=dev
+
+# Logging
+
+export logging_absolute_filepath
+
+# MySQL
+
+export mysql_user=root
+export mysql_pass=dev
+export mysql_address=127.0.0.1:3306
+export mysql_database=mysql
