3eaaf9a2510b30aa75216e400f26a1a08b967f84
freitzzz <1160907@isep.ipp.pt>
2021-07-28 19:53:38 +0100

    Updated load dev env script to include aes cipher block key for task summary encryption

--- tasks/scripts/load_dev_env.sh
+++ tasks/scripts/load_dev_env.sh
@@ -14,3 +14,7 @@ export mysql_user=root
 export mysql_pass=dev
 export mysql_address=127.0.0.1:3306
 export mysql_database=mysql
+
+# AES Cipher
+
+export task_summary_aes_key=asmalltinykeythatisultrasecureee
