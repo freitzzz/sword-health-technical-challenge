83e47b7faa148a74040fe644cb4b1150411234a4
freitzzz <1160907@isep.ipp.pt>
2021-07-28 10:13:55 +0100

    Register middlewares on server start

--- tasks/cmd/tasks/main.go
+++ tasks/cmd/tasks/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"github.com/freitzzz/sword-health-technical-challenge/tasks/internal/data"
 	http "github.com/freitzzz/sword-health-technical-challenge/tasks/internal/http"
+	"github.com/freitzzz/sword-health-technical-challenge/tasks/internal/logging"
 	"github.com/labstack/echo/v4"
 )
 
@@ -9,6 +11,18 @@ func main() {
 
 	// Echo instance
 	e := echo.New()
+
+	db, oerr := data.OpenDbConnection()
+
+	if oerr != nil {
+
+		logging.LogError("Could not open DB connection on server start")
+		logging.LogError(oerr.Error())
+
+		panic("DB connection is required to serve HTTP calls")
+	}
+
+	http.RegisterMiddlewares(e, db)
 
 	http.RegisterHandlers(e)
 
