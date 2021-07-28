b9b45dfeb7023aee5e229e38e623b570e1ed7792
freitzzz <1160907@isep.ipp.pt>
2021-07-28 23:13:50 +0100

    Prepared load dev env script for rabbitmq environment variables

--- notifications/scripts/load_dev_env.sh
+++ notifications/scripts/load_dev_env.sh
@@ -14,3 +14,9 @@ export mysql_user=root
 export mysql_pass=dev
 export mysql_address=127.0.0.1:3306
 export mysql_database=mysql
+
+# RabbitMQ
+
+export rabbitmq_user=root
+export rabbitmq_pass=dev
+export rabbitmq_address=127.0.0.1:5672
