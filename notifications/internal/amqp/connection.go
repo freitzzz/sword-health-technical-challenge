5628be26435bddcdf9153463bc07960b17a2fcea
freitzzz <1160907@isep.ipp.pt>
2021-07-28 23:12:42 +0100

    Connection for rabbit mq

---
+++ notifications/internal/amqp/connection.go
@@ -0,0 +1,25 @@
+package data
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/streadway/amqp"
+)
+
+const (
+	rabbitMQUsernameEnvKey = "rabbitmq_user"
+	rabbitMQPasswordEnvKey = "rabbitmq_pass"
+	rabbitMQAddressEnvKey  = "rabbitmq_address"
+)
+
+var (
+	rabbitMQUsername = os.Getenv(rabbitMQUsernameEnvKey)
+	rabbitMQPassword = os.Getenv(rabbitMQPasswordEnvKey)
+	rabbitMQAddress  = os.Getenv(rabbitMQAddressEnvKey)
+)
+
+func OpenDbConnection() (*amqp.Connection, error) {
+
+	return amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s/", rabbitMQUsername, rabbitMQPassword, rabbitMQAddress))
+}
