440de807355482949698ee701d01a82f01f5c645
freitzzz <1160907@isep.ipp.pt>
2021-07-28 11:45:42 +0100

    Moved responses serialization to responses.go

--- tasks/internal/http/responses.go
+++ tasks/internal/http/responses.go
@@ -1,5 +1,11 @@
 package http
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
 type InvalidParam struct {
 	Message string
 	Type    string
@@ -13,3 +19,23 @@ var (
 		Name:    paginationIndex,
 	}
 )
+
+func InvalidParamBadRequest(c echo.Context, ip InvalidParam) error {
+	return c.JSON(http.StatusBadRequest, ip)
+}
+
+func InternalServerError(c echo.Context) error {
+	return c.NoContent(http.StatusInternalServerError)
+}
+
+func NotAuthorized(c echo.Context) error {
+	return c.NoContent(http.StatusUnauthorized)
+}
+
+func NotFound(c echo.Context) error {
+	return c.NoContent(http.StatusNotFound)
+}
+
+func NoContent(c echo.Context) error {
+	return c.NoContent(http.StatusNoContent)
+}
