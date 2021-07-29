8c1bc1cd59d31df230b6a09f530cb45b10c74026
freitzzz <1160907@isep.ipp.pt>
2021-07-29 15:51:24 +0100

    User Auth request body model

---
+++ auth/internal/http/requests.go
@@ -0,0 +1,9 @@
+package http
+
+// Tooled with json-go-struct mapper: https://mholt.github.io/json-to-go/
+
+type UserAuth struct {
+	Identifier string `json:"id"`
+	Secret     string `json:"secret"`
+}
+}
