62029d9f077576de1d43971ac3dd949e5dbb9114
freitzzz <1160907@isep.ipp.pt>
2021-07-29 13:57:05 +0100

    Load dev environments script for auth service

---
+++ auth/scripts/load_dev_env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# General
+
+export environment=dev
+
+# HTTP
+
+export server_host=
+export server_port=82
+
+# Logging
+
+export logging_absolute_filepath
+
+# JWT
+
+export jwt_auth=HS256
+export jwt_secret=asmalltinykeythatisultrasecureee
