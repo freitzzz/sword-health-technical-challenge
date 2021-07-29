f1fba6ca4ef71b5a15c92711d357d17cc3b8a624
freitzzz <1160907@isep.ipp.pt>
2021-07-29 14:24:28 +0100

    MySQL env keys for auth service

--- auth/scripts/load_dev_env.sh
+++ auth/scripts/load_dev_env.sh
@@ -9,6 +9,13 @@ export environment=dev
 export server_host=
 export server_port=82
 
+# MySQL
+
+export mysql_user=root
+export mysql_pass=dev
+export mysql_address=127.0.0.1:3306
+export mysql_database=mysql
+
 # Logging
 
 export logging_absolute_filepath
